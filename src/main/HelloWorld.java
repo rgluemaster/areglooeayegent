b23f6354efa23e245de01a4e583b114255c8526f
Sebastian Ånerud <anerud@student.chalmers.se>
2013-12-04 20:48:49 +0100

    Derp

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
         System.out.println("Herp");
+        System.out.println("Derp");
 	}
 
 }
